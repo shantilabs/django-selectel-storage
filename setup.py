19079eeaf8b03c4f2a38e6375b6ec7b7c4e0c9d4
Maxim Oransky <maxim.oransky@gmail.com>
2016-10-10 14:44:54 +0300

    ImportError: No module named management.commands fix

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from distutils.core import setup
 
 setup(
     name='django-selectel-storage',
-    version='1.2',
+    version='1.3',
     author='Maxim Oransky',
     author_email='maxim.oransky@gmail.com',
     url='https://github.com/shantilabs/django-selectel-storage',
     packages=[
         'django_selectel',
+        'django_selectel.management',
         'django_selectel.management.commands',
     ],
 )
