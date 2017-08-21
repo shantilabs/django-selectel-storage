2ce6f2ab92200eb60819321dfcb65f761c1fac9e
Maxim Oransky <maxim.oransky@gmail.com>
2017-08-21 15:15:37 +0300

    build removed

--- build/lib/django_selectel/management/commands/pg_backup.py
+++
@@ -1,7 +0,0 @@
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        from ...backup import pg_backup
-        pg_backup()
